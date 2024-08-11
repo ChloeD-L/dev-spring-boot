8b9c9a84aa51f845396306a1a77060b5e8e83b8b
ChloeD-L <linduan1018@gmail.com>
2024-08-11 19:15:38 +1000

    RestAPI

--- src/test/java/com/udemy/devspringboot/DevSpringBootApplicationTests.java
+++ src/test/java/com/udemy/devspringboot/DevSpringBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DevSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
